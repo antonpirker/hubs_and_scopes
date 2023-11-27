1b6b1a054b836ef5f866dc16689c61902e7fc05c
Anton Pirker <anton.pirker@sentry.io>
2023-11-27 15:16:06 +0100

    some comments

--- scope.py
+++ scope.py
@@ -10,6 +10,39 @@ def copy_on_write(property_name):
     """
     Decorator that implements copy on write on a property of a class.
     """
+    # Stuff that write on scope right now:
+    # - Hub.configure_scope writes the propagation context to the scope (if not existing on scope)
+    # - Hub.start_session writes the session to the scope 
+    # - Hub.end_session changes data on the scope
+    # - Hub.stop_auto_session_tracking changes data on the scope
+    # - Hub.resume_auto_session_tracking changes data on the scope
+    #
+    # - Scope.set_new_propagation_context set self._propagation_context  # type: Optional[Dict[str, Any]]
+    # - Scope.generate_propagation_context set self._propagation_context  # type: Optional[Dict[str, Any]]
+    # - Scope.get_dynamic_sampling_context set self._dynamic_sampling_context  # type: Optional[Dict[str, Any]]
+    # - Scope.clear reset all local vars
+    # - Scope.level (_attr_setter) sets self._level  # type: int
+    # - Scope.set_level sets self._level  # type: int
+    # - Scope.fingerprint (_attr_setter) sets self._fingerprint  # type: Optional[List[str]]
+    #( - Scope.transaction sets self._span.containing_transaction  # type: Optional[Span]) because changes span, not scope per se
+    # - Scope.set_transaction_name sets self._transaction  # type: Optional[str] and self._span.containing_transaction.name (and self._span.containing_transaction.source)
+    # - Scope.user (_attr_setter) sets self._user  # type: Optional[User]
+    # - Scope.set_user sets self._user  # type: Optional[User] and updates self._session
+    # - Scope.span (@span.setter) sets self._span  # type: Optional[Span]
+    # - Scope.profile (@profile.setter) sets self._profile  # type: Optional[Profile]
+    # - Scope.set_tag sets self._tags  # type: Dict[str, str]
+    # - Scope.remove_tag pops from self._tags  # type: Dict[str, str]
+    # - Scope.set_context sets self._contexts  # type: Dict[str, Any]
+    # - Scope.remove_context pops from self._contexts  # type: Dict[str, Any]
+    # - Scope.set_extra sets self._extra  # type: Dict[str, Any]
+    # - Scope.remove_extra pops from self._extra  # type: Dict[str, Any]
+    # - Scope.clear_breadcrumbs sets self._breadcrumbs  # type: Deque[Breadcrumb]
+    # - Scope.add_attachment appends to self._attachments  # type: List[Attachment]
+    # - Scope.add_event_processor appends to self._event_processors  # type: List[EventProcessor]
+    # - Scope.add_error_processor appends to self._error_processors  # type: List[ErrorProcessor]
+    # - Scope.update_from_scope updates local vars from other given scope
+    # - Scope.update_from_kwargs updates local vars from given kwargs
+
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -21,7 +54,8 @@ def copy_on_write(property_name):
                 id(getattr(self, property_name)) == id(getattr(current_scope, property_name))
             )
             if same_property_different_scope:
-                # TODO: need to check if shallow copy is ok here. (Check all writable attributes in Scope class)
+                # Probably a deep copy is better, because some attributes reference non-primitive types. (Breadcrumbs, Attachments, EventProcessor, ErrorProcessor)
+                # see big comment above.
                 setattr(self, property_name, copy(getattr(self, property_name)))
 
             return func(*args, **kwargs)
