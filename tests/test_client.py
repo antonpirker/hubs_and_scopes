9fb1b40bd09187e59206131e2037c589c556257c
Anton Pirker <anton.pirker@sentry.io>
2023-11-30 12:47:04 +0100

    More client tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -58,20 +58,23 @@ def test_client_on_scope_without_init():
 
 @pytest.mark.forked
 def test_set_client_init_current():
-    custom_client = sentry_sdk.Client()
-
     sentry_sdk.init()
-
+    initial_client = sentry_sdk.get_client()
+    
     current_scope = sentry_sdk.get_current_scope()
     isolation_scope = sentry_sdk.get_isolation_scope()
     global_scope = sentry_sdk.get_global_scope()
 
+    custom_client = sentry_sdk.Client()
     current_scope.set_client(custom_client)
 
     assert current_scope.client == custom_client
+
     assert isolation_scope.client is None
+    
     assert global_scope.client is not None
-    assert global_scope.client != custom_client  # the one created in init()
+    assert global_scope.client != custom_client 
+    assert global_scope.client == initial_client
 
     client = sentry_sdk.get_client()
     assert client == custom_client
@@ -79,20 +82,23 @@ def test_set_client_init_current():
 
 @pytest.mark.forked
 def test_set_client_init_isolation():
-    custom_client = sentry_sdk.Client()
-
     sentry_sdk.init()
+    initial_client = sentry_sdk.get_client()
 
     current_scope = sentry_sdk.get_current_scope()
     isolation_scope = sentry_sdk.get_isolation_scope()
     global_scope = sentry_sdk.get_global_scope()
 
+    custom_client = sentry_sdk.Client()
     isolation_scope.set_client(custom_client)
 
     assert current_scope.client is None
+
     assert isolation_scope.client == custom_client
+
     assert global_scope.client is not None
-    assert global_scope.client != custom_client  # the one created in init()
+    assert global_scope.client != custom_client
+    assert global_scope.client == initial_client
 
     client = sentry_sdk.get_client()
     assert client == custom_client
@@ -100,20 +106,55 @@ def test_set_client_init_isolation():
 
 @pytest.mark.forked
 def test_set_client_init_isolation():
-    custom_client = sentry_sdk.Client()
-
     sentry_sdk.init()
+    initial_client = sentry_sdk.get_client()
 
     current_scope = sentry_sdk.get_current_scope()
     isolation_scope = sentry_sdk.get_isolation_scope()
     global_scope = sentry_sdk.get_global_scope()
 
+    custom_client = sentry_sdk.Client()
     global_scope.set_client(custom_client)
 
     assert current_scope.client is None
+
     assert isolation_scope.client is None
+
     assert global_scope.client is not None
     assert global_scope.client == custom_client
+    assert global_scope.client != initial_client
 
     client = sentry_sdk.get_client()
     assert client == custom_client
+    assert client != initial_client
+
+
+@pytest.mark.forked
+def test_set_multiple_clients():   
+    sentry_sdk.init()
+    initial_client = sentry_sdk.get_client()
+
+    current_client = sentry_sdk.Client()
+    isolation_client = sentry_sdk.Client()
+    global_client = sentry_sdk.Client()
+
+    current_scope = sentry_sdk.get_current_scope()
+    isolation_scope = sentry_sdk.get_isolation_scope()
+    global_scope = sentry_sdk.get_global_scope()
+
+    current_scope.set_client(current_client)
+    isolation_scope.set_client(isolation_client)
+    global_scope.set_client(global_client)
+
+    assert current_scope.client == current_client
+    assert current_scope.client != initial_client
+
+    assert isolation_scope.client == isolation_client
+    assert isolation_scope.client != initial_client
+
+    assert global_scope.client == global_client
+    assert global_scope.client != initial_client
+
+    client = sentry_sdk.get_client()
+    assert client == current_client
+    assert client != initial_client
